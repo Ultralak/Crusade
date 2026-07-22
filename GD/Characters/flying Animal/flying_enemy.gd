00158bc155633604f092320c654d4ec9b05a9108
David Oluwatimilehin Adekeye <david07adekeye@gmail.com>
2026-07-22 18:14:44 +0200

    commit 2.7
    finished base player detection

--- GD/Characters/flying Animal/flying_enemy.gd
+++ GD/Characters/flying Animal/flying_enemy.gd
@@ -4,5 +4,6 @@ extends CharacterBody2D
 @export var max_health : float  = 10.0
 @export var damage_amount : float  = 4
 @export var detection_radius : float = 50
+@export var max_chase_distance : float = 1000
 var knockback_dir : Vector2
 @export var knockback_force : float
--- GD/Characters/flying Animal/player_detection_component.gd
+++ GD/Characters/flying Animal/player_detection_component.gd
@@ -6,39 +6,50 @@ class_name PlayerDetectionComponent
 @export var enemy : CharacterBody2D
 @export var raycast : RayCast2D
 var target : CharacterBody2D
-var is_target_setup  : bool = false
 var detection_radius : float
 var distance_to_player : float
-var has_seen_player : bool = false
-var is_out_of_bounds : bool 
+var max_chase_distance : float
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	detection_radius = enemy.detection_radius
+	max_chase_distance  = enemy.max_chase_distance
 	raycast.enabled = false
+	if detection_radius >= max_chase_distance:
+		printerr("Detection radius is bigger than max distance")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	if FSM.current_node_state_name != "idle" and:
+func _physics_process(_delta: float) -> void:
+	if FSM.current_node_state_name != "idle" :
+		if is_out_of_bounds():
+				FSM.transition_to("idle")
 		return
-	if !is_target_setup:
-		target = PlayerManager.player
-		is_target_setup = true
 	else:
-		distance_to_player = enemy.global_position.distance_to(target.global_position)
-		if distance_to_player <= detection_radius:
-			raycast.enabled = true
-			raycast.target_position = target.global_position
-			if raycast.is_colliding():
-				if raycast.get_collider() is Character:
-					raycast.enabled = false
-					has_seen_player = true
-					FSM.transition_to("chase")
+		if !target:
+			target = PlayerManager.player
+		else :
+			distance_to_player = enemy.global_position.distance_to(target.global_position)
+			if distance_to_player <= detection_radius  :
+				raycast.enabled = true
+				raycast.target_position = target.global_position - raycast.global_position
+				raycast.force_raycast_update()
+				if raycast.is_colliding():
+					#print("Object_name : %s" % [raycast.get_collider().get_class()])
+					if raycast.get_collider() is Character:
+						print("true")
+						raycast.enabled = false
+						FSM.transition_to("chase")
+					else:
+						pass
 				else:
+					#print("not colliding")
 					pass
-		else:
-			raycast.enabled = false
+			else:
+				
+				raycast.enabled = false
 	
 	
-
+func is_out_of_bounds():
+	if target:
+		return enemy.global_position.distance_to(target.global_position) > max_chase_distance
 
 	
